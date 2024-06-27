febdb9c6dd117291d1b9c5eb9cc5962cd599b556
argmike <mike@108capital.ltd>
2024-06-27 18:21:35 +0545

    Agent workflow to use sequence of tools

--- core/agents.py
+++ core/agents.py
@@ -6,7 +6,9 @@ from langchain.schema import AgentFinish
 from langchain_core.runnables.base import RunnableSequence
 from langchain_core.runnables.utils import Output
 from langchain_openai import ChatOpenAI
-from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+from langchain.agents.format_scratchpad import format_to_openai_functions
 
 class Agent:
     def __init__(self, role, goal, verbose=True):
@@ -21,22 +23,52 @@ class Agent:
         return ChatOpenAI(model="gpt-3.5-turbo", temperature=0, max_retries=3)
 
 class Task:
-    def __init__(self, description: str, agent: Agent, tools: dict[str: Callable]):
+    def __init__(self, description: str, outcome: str, agent: Agent, tools: dict[str: Callable]):
         self.description = description
+        self.outcome = outcome
         self.agent = agent
         self.tools = tools
 
     def execute(self):
-        llm_with_tools = self.agent.llm.bind_tools(list(self.tools.values()))
-        prompt = f"""
-        You are a {self.agent.role}. Your goal is to {self.agent.goal}. Perform the given task. Task: {self.description}
         """
-        llm_chain: RunnableSequence = llm_with_tools | OpenAIToolsAgentOutputParser()
-        res: Output = llm_chain.invoke(prompt)
+        Tasks through chain of agents. Call this for general tasks.
+        """
+        history = []
 
-        if self.agent.verbose:
-            pprint(res)
-            print("-------------------")
+        llm_with_tools = self.agent.llm.bind_functions(list(self.tools.values()))
 
-        if isinstance(res, AgentFinish): return res.return_values['output']
-        else: return self.tools[res[0].tool].run(res[0].tool_input)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "You are a {role}. Your goal is to {goal}. Use the provided tools appropriately to accomplish tasks."
+                ),
+                ("human", "Perform the given task. Task: {description}. The desired outcome is {outcome}. Use the necessary tools."),
+                MessagesPlaceholder(variable_name="history")
+            ]
+        )
+
+        llm_chain: RunnableSequence = prompt | llm_with_tools | OpenAIFunctionsAgentOutputParser()
+
+        while True:
+            fmt_openai_functions = format_to_openai_functions(history)
+            res = llm_chain.invoke(
+                {
+                    "role": self.agent.role,
+                    "goal": self.agent.goal,
+                    "description": self.description,
+                    "outcome": self.outcome,
+                    "history": fmt_openai_functions,
+                }
+            )
+
+            if self.agent.verbose:
+                pprint(res)
+                print("-------------------")
+
+            if isinstance(res, AgentFinish): 
+                return res.return_values['output']
+
+            else: 
+                tool_res = self.tools[res.tool].run(res.tool_input)
+                history.append((res, tool_res))
