505c2cba96d2c5aec690ea1460020d8b2497c6f6
argmike <mike@108capital.ltd>
2024-06-27 18:18:05 +0545

    Add a translator

--- core/tools.py
+++ core/tools.py
@@ -14,6 +14,13 @@ def multiply(a: int, b: int) -> str:
 
 @tool
 def random_joke_from_openai(topic: str) -> str:
-    """A comedian who can perform good jokes."""
+    """A comedian who can perform good jokes in English."""
     llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=1.1)
-    return (llm | StrOutputParser()).invoke(f"Tell me a random funny joke on {topic}")
+    return (llm | StrOutputParser()).invoke(f"Tell me a random funny joke on {topic} in English")
+
+@tool
+def translator_from_openai(text: str, lang: str) -> str:
+    """A translator that translates a given text into the specified language."""
+    prompt = f"Translate the following text into {lang}: '{text}'"
+    llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.3)
+    return (llm | StrOutputParser()).invoke(prompt)
